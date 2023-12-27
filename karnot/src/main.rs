efb6a73a67ed7fd191a335d2b74493b58afbee2b
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2023-12-27 13:27:36 +0530

    rebase

--- karnot/src/main.rs
+++ karnot/src/main.rs
@@ -26,9 +26,9 @@ fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Some(Commands::Init) => karnot::cli::init::init(),
-        Some(Commands::List) => karnot::cli::list::list(),
-        Some(Commands::Run { app_chain }) => karnot::cli::run::run(app_chain),
+        Some(Commands::Init) => cli::init::init(),
+        Some(Commands::List) => cli::list::list(),
+        Some(Commands::Run { app_chain }) => cli::run::run(app_chain),
         None => println!("Use --help to see the complete list of available commands"),
     }
 }
