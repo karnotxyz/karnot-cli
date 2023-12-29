c167e904638c6972b3f69846d417a604adf86849
Anshal Shukla <shukla.anshal85@gmail.com>
2023-12-29 12:19:25 +0530

    add: run command

--- templates/karnot.sh
+++ templates/karnot.sh
@@ -5,9 +5,9 @@ set -x #echo on
 BASE_PATH={{BASE_PATH}}
 
 # setup node
-cargo run --release -- setup --chain=dev --from-remote --base-path=$BASE_PATH
+#cargo run --release -- setup --chain=dev --from-remote --base-path=$BASE_PATH
 
-madara \
+$HOME/.karnot/madara/target/debug/madara \
   --base-path=$BASE_PATH \
   --rpc-cors=all \
   --chain=dev \
